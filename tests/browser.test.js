355c0dae7c98fb5cd3b374e942f02f7c35a304d1
georgGrankvist <62744506+georgGrankvist@users.noreply.github.com>
2022-05-06 15:24:24 +0200

    failing test cases

---
+++ tests/browser.test.js
@@ -0,0 +1,42 @@
+const { expect } = require('chai');
+const { Builder, By, until } = require('selenium-webdriver');
+require('geckodriver');
+
+const fileUnderTest = 'file://' + __dirname.replace(/ /g, '%20') + '/../dist/index.html';
+const defaultTimeout = 10000;
+let driver;
+//jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5; // 5 minuter
+
+// Det här körs innan vi kör testerna för att säkerställa att Firefox är igång
+beforeAll(async () => {
+console.log(fileUnderTest);
+    driver = await new Builder().forBrowser('firefox').build();
+    await driver.get(fileUnderTest);
+});
+
+// Allra sist avslutar vi Firefox igen
+afterAll(async() => {
+    await driver.quit();
+}, defaultTimeout);
+
+test('The stack should be empty in the beginning', async () => {
+	let stack = await driver.findElement(By.id('top_of_stack')).getText();
+	expect(stack).equal("n/a");
+});
+
+test ('Peek should show n/a after peek on empty stack', async () => {
+    let peek = await driver.findElement(By.id ('peek'));
+    let stack = await driver.findElement(By.id('top_of_stack')).getText();
+    await peek.click(); 
+    expect(stack).equal("hej");
+})
+
+describe('Clicking "Pusha till stacken"', () => {
+	it('should open a prompt box', async () => {
+		let push = await driver.findElement(By.id('push'));
+		await push.click();
+		let alert = await driver.switchTo().alert();
+		await alert.sendKeys("Bananer");
+		await alert.accept();
+	});
+});
